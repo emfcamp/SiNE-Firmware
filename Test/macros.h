cda1c269241e8aab915fff442e0401cd01c537d2
HAC Manchester <hacman@hacman-1>
2013-03-07 20:45:46 +0000

    SiNE Badge test firmware v0.1

---
+++ Test/macros.h
@@ -0,0 +1,28 @@
+/*
+ * macros.h - Macros
+ * Copyright (C) 2011  Bob Clough <bob@clough.me>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#define bit_get(p,m) ((p) & (m))
+#define bit_set(p,m) ((p) |= (m))
+#define bit_clear(p,m) ((p) &= ~(m))
+#define bit_flip(p,m) ((p) ^= (m))
+#define bit_write(c,p,m) (c ? bit_set(p,m) : bit_clear(p,m))
+#define BIT(x) (0x01 << (x))
+#define LONGBIT(x) ((unsigned long)0x00000001 << (x))
+
+#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) 
---
+++ Test/sinetest.h
@@ -0,0 +1,27 @@
+#define TRUE 1
+#define FALSE 0
+
+#define SR_A_PORT PORTA
+#define SR_A_PIN BIT(0)
+
+#define SR_CLK_PORT PORTA
+#define SR_CLK_PIN BIT(1)
+
+#define SR_CLR_PORT PORTA
+#define SR_CLR_PIN BIT(2)
+
+#define LED_C5_PORT PORTA
+#define LED_C5_PIN BIT(3)
+
+#define SCK_PORT PORTA
+#define SCK_PIN BIT(4)
+
+#define MISO_PORT PORTA
+#define MISO_PIN BIT(5)
+
+#define MOSI_PORT PORTA
+#define MOSI_PIN BIT(6)
+
+#define IR_PWM_PORT PORTA
+#define IR_PWM_PIN BIT(7)
+
