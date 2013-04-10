591071a435f5d7390a8d45abf129fcecb3921bc6
Jim MacArthur <jim-git@mode7.co.uk>
2013-04-10 21:27:00 +0100

    IR receive code now recognises RC-5

--- Test/sinetest.c
+++ Test/sinetest.c
@@ -16,12 +16,66 @@ char a[4] = { 0b10001, 0b11111, 0b01010, 0b00100 };
 char b[4] = { 0b11111, 0b10001, 0b11110, 0b10000 };
 
 char* colData;
-
+volatile int IRcode = 0;
+int latchIR = 0;
+char currentBit = 1;
+unsigned int oldTime = 0;
+char interbit = 0;
+volatile int seq = 0;
+const int tolerance = 200;
+int receives = 0;
+int elapsed = 0;
 
 ISR(PCINT0_vect)
 {
-	column = 0;
-	row = 0;
+	// Check the clock and figure out the time since the last change
+	// If it's "long", we're on the start bit, and the current bit is 1, emit 1.
+	// If it's about 889usec and interbit is set, clear interbit.
+	// If it's about 889usec, emit current, set the interbit flag.
+	// If it's longer, about 1778usec, current=!current and emit current.
+
+	unsigned int time = TCNT1;
+	if(time < oldTime) {
+		unsigned int remain = 0xFFFF - oldTime;
+		elapsed += time + remain;
+	}
+	else {
+		elapsed += time - oldTime;
+	}
+	oldTime = time;
+
+	if(elapsed < 889+tolerance && seq < 14) {
+		if(interbit) {
+			interbit = 0;
+		} else {
+			IRcode <<= 1;
+			IRcode |= currentBit;
+			interbit = 1;
+			seq ++;
+			if(seq == 14) {
+				latchIR = IRcode;
+			}
+		}
+		elapsed = 0;
+	}
+	else if(elapsed > 1778-tolerance && elapsed < 1778+tolerance && seq < 14) {
+		currentBit = 1-currentBit;
+		IRcode <<= 1;
+		IRcode |= currentBit;
+		seq ++;
+		elapsed = 0;
+		if(seq == 14) {
+			latchIR = IRcode;
+		}
+
+	}
+	else if(elapsed > 2000) {
+		interbit = 0;
+		IRcode = 1;
+		currentBit = 1;
+		seq=0;
+		elapsed = 0;
+	}
 }
 
 void setShift(int value) 
@@ -66,7 +120,7 @@ void setup()
 	// Configure IR_OUT pwm
 	// Make MISO an output and drive it high
 	bit_set(MISO_DDR, MISO_PIN); 
-	bit_set(MISO_PORT, MISO_PIN);
+	bit_clear(MISO_PORT, MISO_PIN);
 
 	// Make IR_PWM an output
 	bit_set(IR_PWM_DDR, IR_PWM_PIN);
@@ -89,6 +143,12 @@ void setup()
 
 	bit_set(BTN1_PORT, BTN1_PIN);
 	bit_set(BTN2_PORT, BTN2_PIN);
+
+	TCNT1H = 0; // Initial TIMER1
+	TCNT1L = 0; // Initial TIMER1
+	// Set up timer1 as a general interval timer.
+	// Ideally we should set up timer1 so it latches at TOP
+	TCCR1B = 2; // Start clock (/8 prescaler = 1Mhz)
 }
 
 void loop()
@@ -102,7 +162,15 @@ void loop()
 		colData = b;
 	}
 
-	bit_flip(MISO_PORT, MISO_PIN);
+	int command = (latchIR & 0x3F);
+	int address = (latchIR >> 5) & 0x1f;
+
+	colData[2] = command & 0x1F; 
+	colData[3] = address;
+
+	colData[0] = 1 << (seq % 5);
+	colData[1] = 0x1;
+	//bit_flip(MISO_PORT, MISO_PIN); // Uncomment this to flash the IR Led
 	for(row=0;row<4;row++) {
 		int columns = colData[row];
 		int rows = 1<<(row % 4);
