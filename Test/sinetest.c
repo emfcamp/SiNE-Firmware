94a0a19d0447300bfa673067aa74dacca76a5d6a
Jim MacArthur <jim-git@mode7.co.uk>
2013-05-04 11:39:48 +0100

    Moved common config out to its own file

--- Test/sinetest.c
+++ Test/sinetest.c
@@ -10,6 +10,7 @@
 #include "sinetest.h"
 
 #include "display.h"
+#include "common_config.h"
 
 volatile int column;
 volatile int row;
@@ -20,7 +21,6 @@ int latchIR = 0;
 char currentBit = 1;
 unsigned int oldTime = 0;
 volatile int seq = 0;
-int elapsed = 0;
 int bit = 0;
 unsigned int foundBeacons = 0;
 unsigned int foundBeacons2 = 0;
@@ -38,6 +38,7 @@ ISR(PCINT0_vect)
         const int tolerance = 200;
         const int timebase = 600;
 	unsigned int time = TCNT1;
+	int elapsed; 
         elapsed = 0;
 	if(time < oldTime) {
 		unsigned int remain = 0xFFFF - oldTime;
@@ -75,71 +76,28 @@ ISR(PCINT0_vect)
 
 void setup()
 {
-	// Set shift register pins to output
-	bit_set(SR_CLK_DDR, SR_CLK_PIN);
-	bit_set(SR_CLK_DDR, SR_CLR_PIN);
-	bit_set(SR_CLK_DDR, SR_A_PIN);
+	general_pin_setup();
 
-	// Set Column 5 to output and set it low
-	bit_set(SR_CLK_DDR, LED_C5_PIN);
-	bit_clear(SR_CLK_PORT, LED_C5_PIN);
+	configure_IR_interrupt();
 
-	// Pulse shift register's !CLR pin high then low to clear the SR
-	bit_clear(SR_CLR_PORT, SR_CLR_PIN);
-	_delay_ms(1);	
-	bit_set(SR_CLR_PORT, SR_CLR_PIN);
-	_delay_ms(1);	
+	configure_IR_output();
 
-	column = 0;
-	row = 0;
-	
-	bit_clear(MOSI_DDR, MOSI_PIN); // Make sure MOSI is an input
-
-	// Configure interrupts
-	PCMSK0 |= (1<<PCINT6); // Interrupt on pin 6 change (MOSI)
-	GIMSK |= (1<<PCIE0); // Enable interrupts on PCINT7:0
-	sei();  // Enable global interrupts.
-
-	// Configure IR_OUT pwm
-	// Make MISO an output and drive it high
-	bit_set(MISO_DDR, MISO_PIN); 
-	bit_clear(MISO_PORT, MISO_PIN);
-
-	// Make IR_PWM an output
-	bit_set(IR_PWM_DDR, IR_PWM_PIN);
-
-	// We run at 8MHz
-	// We want period to be half the wavelength
-	// We want 38kHz = 1/38000 seconds / cycle
-	// = 1/76000 seconds/half cycle
-	// = 8000000/76000
-
-	TCNT0 = 0; // Initial clock
-	int period = 106; // Gets about 38kHz
-	OCR0B = period; // OC0B compare register
-	OCR0A = period; // Same value
-	
-	TCCR0A |= (1<<COM0B0) | (1<<WGM01); // Toggle OC0B on compare match, CTC mode
-	TCCR0B = 1; // start clock (/1 prescaler)
-
-
-	bit_set(BTN1_PORT, BTN1_PIN);
-	bit_set(BTN2_PORT, BTN2_PIN);
-
+	// Set up timer1 as a general interval timer.
 	TCNT1H = 0; // Initial TIMER1
 	TCNT1L = 0; // Initial TIMER1
-	// Set up timer1 as a general interval timer.
-	// Ideally we should set up timer1 so it latches at TOP
 	TCCR1B = 2; // Start clock (/8 prescaler = 1Mhz)
 
+	// Read persistent variables from EEPROM
         foundBeacons = eeprom_read_byte((char*)0) | (eeprom_read_byte((char*)1)<<8);
         foundBeacons2 = eeprom_read_byte((char*)2) | (eeprom_read_byte((char*)3)<<8);
-        frame = 0;
-
 	badgeID = eeprom_read_byte((char*)4) | ((eeprom_read_byte((char*)5) & 1)<<8);
 
+	// Set up other global variables
         idTimeout = 4;
         eraseConfirm = 0;
+	column = 0;
+	row = 0;
+        frame = 0;
 }
 
 void transmitBadgeID()
